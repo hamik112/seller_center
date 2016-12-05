9e123f8622fcf8c9f5a099fed549db1f1aaa906c
wlwood <wl_wood@163.com>
2016-12-05 02:14:22 +0000

    requirement.txt

--- SellerCenter/settings.py
+++ SellerCenter/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "seller_center",
-        "USER": "ROOT",
+        "USER": "root",
         "PASSWORD": "admin",
         "HOST": "",
         "PORT": ""
