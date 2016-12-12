404a9d07fecf02e5e825600706704e7f6a95816f
wlwood <lijun@starmerx.com>
2016-12-12 20:15:24 +0800

    添加model

--- center/models.py
+++ center/models.py
@@ -8,6 +8,7 @@ from django.utils import  timezone
 
 
 class GenerateReport(models.Model):
+    """ date range reports 请求记录 """
     id = models.AutoField(primary_key=True, null=False)
     is_custom        =  models.CharField( max_length= 30, default="", null=False)
     reportType       =  models.CharField( max_length= 30, default="", null=False)       #report 类型,
@@ -28,5 +29,59 @@ class GenerateReport(models.Model):
 
     def __unicode__(self):
         return self.reportType
+
+
+
+
+class AllStatements(models.Model):
+
+    id           =  models.AutoField(primary_key=True, null=False)
+    username     =  models.CharField( max_length= 200, default= "", null=False)
+
+    settlement_period     = models.CharField( max_length=150, default="", null=False)
+    beginning_balance     = models.CharField( max_length=100, default="", null=False)
+    product_charges_total = models.CharField( max_length=100, default="", null=False)
+    promo_retates_total   = models.CharField( max_length=100, default="", null=False)
+    amazon_fees_total     = models.CharField( max_length=100, default="", null=False)
+    other_total           = models.CharField( max_length=100, default="", null=False)
+    deposit_total         = models.CharField( max_length=100, default="", null=False)
+
+    date_time             = models.DateTimeField(default=timezone.now())
+
+    class Meta:
+        db_table = "all_statements_tb"
+    def __unicode__(self):
+        return self.settlement_period
+
+
+class TransactionView(models.Model):
+    id          =   models.AutoField( primary_key=True, null=False)
+    username    =   models.CharField( max_length= 200, default="", null=False)
+
+
+    filter_view  = models.CharField( max_length=200, default="", null=False)
+    transaction_type = models.CharField( max_length=200, default="", null=False)
+    order_id         = models.CharField( max_length=100, default="", null=False)
+    product_details  = models.CharField( max_length=350, default="", null=False)
+    total_product_charges = models.CharField( max_length=100, default="0.00", null=False)
+    total_promotional_rebates = models.CharField( max_length=100, default="0.00", null=False)
+    amazon_fees           = models.CharField( max_length=100, default="0.00", null=False)
+    other                 = models.CharField( max_length=100, default="0.00", null=False)
+    total                 = models.CharField( max_length=100, default="0.00", null=False)
+
+    date_time             = models.DateTimeField(default=timezone.now())
+
+    class Meta:
+        db_table = "transaction_view_tb"
+
+    def __unicode__(self):
+        return self.transaction_type
+
+
+
+
+
+
+
 
 
