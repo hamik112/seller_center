6310c870897fb6e086bad6dbaed0bb52efee0d2e
wlwood <lijun@starmerx.com>
2016-12-14 16:09:00 +0800

    startä¸于后台

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python  change_auth_user_length.py
+python  change_auth_user_length.py &
 
 
 python manage.py celery worker -q -c 4  --loglevel=info  &
 
 
-python manage.py runserver 0.0.0.0:8080
+python manage.py runserver 0.0.0.0:8080 &
 
