c649466209cbb82f40ac02ebc7e5d2859ef4ccde
wlwood <wl_wood@163.com>
2016-12-14 02:43:32 +0000

    修改phantomjs路径

--- center/dataService/phantojs_to_pdf.py
+++ center/dataService/phantojs_to_pdf.py
@@ -1,13 +1,19 @@
 # encoding:utf-8
 
+import os
 
+from django.conf import settings
 from selenium import webdriver
+
+base_dir = settings.BASE_DIR
+
+phantomjs_path = os.path.join(base_dir, "center/dataService/phantomjs")
 
 def execute(driver, script, args):
     driver.execute('executePhantomScript', {'script': script, 'args' : args })
 
 def html_to_pdf(result_path_html , filename):
-    driver = webdriver.PhantomJS('phantomjs')
+    driver = webdriver.PhantomJS(executable_path=phantomjs_path)
     # hack while the python interface lags
 
     driver.command_executor._commands['executePhantomScript'] = ('POST', '/session/$sessionId/phantom/execute')
