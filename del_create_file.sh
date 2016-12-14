83281cb854be6e157cdabf5d1cea0fa0f4eafbea
wlwood <lijun@starmerx.com>
2016-12-14 16:08:26 +0800

    删除了生成的pdfhtml

--- del_create_file.sh
+++ del_create_file.sh
@@ -4,4 +4,6 @@ rmpyc.py .
 rm -rf center/GENERATE_REPORT
 rm -rf center/log
 rm -rf manageApp/UploadFilePath
-rm ghostdriver.log 
+rm  ghostdriver.log
+rm  center/templates/pdf_hml/result_pdf.html
+
