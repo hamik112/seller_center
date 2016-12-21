155d0a3537ace72395e39b0804dd6d2c123fa9d6
wlwood <lijun@starmerx.com>
2016-12-21 13:09:57 +0800

    修改allstatements

--- center/dataService/all_statements.py
+++ center/dataService/all_statements.py
@@ -7,6 +7,17 @@ from center.models import  AllStatements
 from center.dataService.create_xls import is_number
 
 
+def number_format(num):
+    if num != 0:
+        num = str("%.2f"%float(num))
+    else:
+        num = 0
+    if float(num) >= 1000 or float(num) <= -1000:
+        num = format(float(num),",")
+    return str(num)
+
+
+
 class AllStatementsList(object):
     def __init__(self, username):
         self.username = username
@@ -37,6 +48,8 @@ class AllStatementsList(object):
 
     def number_add_dot(self,num):
         if float(num) < 0:
+            num = number_format(num)
             return "-$"+str(num).replace("-","")
         else:
+            num = number_format(num)
             return "$"+str(num)
