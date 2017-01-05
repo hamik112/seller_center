05fda07eacf1f8d589dfd78029bae7d035527df6
wood <wood@debian>
2017-01-05 20:22:39 +0800

    修改地å启动

--- start.sh
+++ start.sh
@@ -8,5 +8,7 @@ nohup python manage.py celery worker -q -c 4  --loglevel=info  &
 
 #python manage.py runserver 0.0.0.0:8080 &
 
-#uwsgi --gevent 100 --gevent-monkey-patch --http :8080 -M --process 4 --wsgi-file start_server.py &  #uwsgi 启动
-nohup uwsgi --gevent 100 --gevent-monkey-patch --http :8009 -M --process 4 --wsgi-file start_server.py &  #uwsgi 启动
+#uwsgi  --http :8080 -M --process 4 --wsgi-file start_server.py &  #uwsgi 启动
+#nohup  --http :8009 -M --process 4 --wsgi-file start_server.py &  #uwsgi 启动
+
+uwsgi -x seller_center_socoket.xml &
