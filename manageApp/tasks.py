5c30f62a3c20f21081bc920ae45ab86359be48f3
cc <l_cc677@163.com>
2017-04-06 16:29:31 +0800

    modefy files

--- manageApp/tasks.py
+++ manageApp/tasks.py
@@ -218,10 +218,10 @@ def read_excel(file_name):
 @task(max_retries=3,default_retry_delay=1 * 6)
 def deal_file(filepath,code,result_filepath,obj):
     try:
-        r=redis.Redis(host='127.0.0.1',port='6379')
-        #data = read_excel('/tmp/source_data.xls' )
+        r = redis.Redis(host='127.0.0.1', port='6379')
+        # data = read_excel('/tmp/source_data.xls' )
         data = read_excel(filepath)
-        data_head=data[0]['values'][:8]
+        data_head = data[0]['values'][:8]
         data_head[7].append('internal_sku')
         data_head[7].append('asin')
         data_head[7].append('hky')
@@ -253,40 +253,39 @@ def deal_file(filepath,code,result_filepath,obj):
             li[20]:other
             li[21]:total
             """
-            internal_sku=r.hget(li[4],'sku')
-            asin=r.hget(li[4],'asin')
+            internal_sku = r.hget(li[4], 'sku')
+            asin = r.hget(li[4], 'asin')
             li.append(internal_sku)
             li.append(asin)
             if internal_sku:
-                if r.hget('asin_'+internal_sku,'fba'):
+                if r.hget('asin_' + internal_sku, 'fba'):
                     li.append('haiyun')
                 else:
                     li.append('kongyun')
             else:
                 li.append('')
-
             if r.get('xh_'+code):
                 li.append(code)
                 li.append(r.get('xh_'+code))
 
-        data_count=len(data_list)
-        order_id_list_len=data_count*3
-        order_id_list=[]
+        data_count = len(data_list)
+        order_id_list_len = data_count * 3
+        order_id_list = []
         for order_id in range(order_id_list_len):
-            order_id_list.append(random.randint(1000000,9999999))
-        order_list=list(set(order_id_list))
+            order_id_list.append(random.randint(1000000, 9999999))
+        order_list = list(set(order_id_list))
 
-        ss=0
-        address_list=[]
+        ss = 0
+        address_list = []
         for li in data_list:
             if li[3]:
-                address=[li[9],li[10],li[11]]
+                address = [li[9], li[10], li[11]]
                 address_list.append(address)
             else:
                 continue
         random.shuffle(address_list)
 
-        address_i=0
+        address_i = 0
         for li in data_list:
             """
             li[0]:date/time
@@ -318,46 +317,55 @@ def deal_file(filepath,code,result_filepath,obj):
             li[26]:wangguandaima
             """
             if li[3]:
-                li[9]=address_list[address_i][0]
-                li[10]=address_list[address_i][1]
-                li[11]=address_list[address_i][2]
+                if li[2] == 'Chargeback Refund' or li[2] == 'A-to-z Guarantee Claim' or li[2] == 'order' or li[2] == 'refund':
+                    if r.get('addr_' + li[3]):
+                        li[9] = eval(r.get('addr_' + li[3]))[0]
+                        li[10] = eval(r.get('addr_' + li[3]))[1]
+                        li[11] = eval(r.get('addr_' + li[3]))[2]
+                    else:
+                        r.set('addr_' + li[3], str(address_list[address_i]))
+                        li[9] = address_list[address_i][0]
+                        li[10] = address_list[address_i][1]
+                        li[11] = address_list[address_i][2]
                 if r.get(li[3]):
-                    li[3]=r.get(li[3])
+                    li[3] = r.get(li[3])
                 else:
-                    old_li3=li[3]
-                    li[3]=li[3][:4] + str(order_list[ss])+'-'
-                    ss+=1
-                    li[3]+= str(order_list[ss])
-                    ss+=1
-                    r.set(old_li3,li[3])
-                li[12]=(round(li[12]*1.01,2))
-                li[21]=(float(li[12])+float(li[13])+float(li[14])+float(li[15])+float(li[16])+float(li[17])+float(li[18])+float(li[19])+float(li[20]))
+                    old_li3 = li[3]
+                    li[3] = li[3][:4] + str(order_list[ss]) + '-'
+                    ss += 1
+                    li[3] += str(order_list[ss])
+                    ss += 1
+                    r.set(old_li3, li[3])
+                li[12] = (round(li[12] * 1.01, 2))
+                li[21] = (
+                float(li[12]) + float(li[13]) + float(li[14]) + float(li[15]) + float(li[16]) + float(li[17]) + float(
+                    li[18]) + float(li[19]) + float(li[20]))
                 if li[22]:
-                    li[4]=r.hget('asin_'+li[23],'newoutsku')
-                if len(li)==25:
+                    li[4] = r.hget('asin_' + li[23], 'newoutsku')
+                if len(li) == 25:
                     print li[24]
-                address_i+=1
+                address_i += 1
             else:
                 continue
 
-        j=0
-        file=xlwt.Workbook()
-        table=file.add_sheet('info',cell_overwrite_ok=True)
+        j = 0
+        file = xlwt.Workbook()
+        table = file.add_sheet('info', cell_overwrite_ok=True)
         for head in data_head:
-            t=0
+            t = 0
             for h in head:
-                table.write(j,t,head[t])
-                t+=1
-            j+=1
+                table.write(j, t, head[t])
+                t += 1
+            j += 1
 
         for li in data_list:
-            t=0
+            t = 0
             for h in li:
-                table.write(j,t,li[t])
-                t+=1
-            j+=1
+                table.write(j, t, li[t])
+                t += 1
+            j += 1
 
-        j=0
+        j = 0
         file.save(result_filepath)
         obj.status = 1
         obj.save()
